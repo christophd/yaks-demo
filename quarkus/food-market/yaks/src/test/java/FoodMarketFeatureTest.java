47a0196773425d6e39be4b312757145c5463896b
Christoph Deppisch <cdeppisch@redhat.com>
2023-12-08 21:12:49 +0100

    chore: Add pretty print Cucumber plugin

--- quarkus/food-market/yaks/src/test/java/FoodMarketFeatureTest.java
+++ quarkus/food-market/yaks/src/test/java/FoodMarketFeatureTest.java
@@ -4,18 +4,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        extraGlue = {
-                "org.citrusframework.yaks.standard",
-                "org.citrusframework.yaks.http",
-                "org.citrusframework.yaks.openapi",
-                "org.citrusframework.yaks.kafka",
-                "org.citrusframework.yaks.groovy",
-                "org.citrusframework.yaks.camel",
-                "org.citrusframework.yaks.camelk",
-                "org.citrusframework.yaks.testcontainers",
-                "org.citrusframework.yaks.selenium"
-        },
-        plugin = { "org.citrusframework.yaks.report.TestReporter" }
+        extraGlue = "org.citrusframework.yaks",
+        plugin = { "pretty", "org.citrusframework.yaks.report.TestReporter" }
 )
 public class FoodMarketFeatureTest {
 }
